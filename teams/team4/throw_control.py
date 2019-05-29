e620cd24c6be98065232424ea7382df0a022314b
wanpiqiu123 <46051943+wanpiqiu123@users.noreply.github.com>
2019-05-29 20:41:30 +0800

    Update throw_control.py

--- teams/team4/throw_control.py
+++ teams/team4/throw_control.py
@@ -1,57 +1,86 @@
 import serial
 import serial.tools.list_ports
 import time
-import urllib.request
+import requests
 import re
 
 
-def movement(lu,rd): #input:left_up and right_down
+def movement(lu,rd,length,width): #input:left_up and right_down
     # x and y
-    x=640
-    y=480
-    shift = 10
+    x=length
+    y=width
+    shift = 50
     center_dot = [(lu[0]+rd[0])/2,(lu[1]+rd[1])/2]
+    # if center_dot[0]>x/2+shift:
+    #     if center_dot[1]>y/2+shift: #right_up
+    #         return 'p'
+    #     elif y/2-shift<center_dot[1]<y/2+shift: #right
+    #         return 'd'
+    #     else: #right_down
+    #         return 'l'
+    # elif center_dot[0]<x/2-shift:
+    #     if center_dot[1]>y/2+shift: #left_up
+    #         return 'o'
+    #     elif y/2-shift<center_dot[1]<y/2+shift: #left
+    #         return 'a'
+    #     else: #left_down
+    #         return 'k'
+    # else:
+    #     if center_dot[1]>y/2+shift: #up
+    #         return 'w'
+    #     elif y/2-shift<center_dot[1]<y/2+shift: #stop
+    #         return
+    #     else: #down
+    #         return 's'
+    print(center_dot)
     if center_dot[0]>x/2+shift:
-        if center_dot[1]>y/2+shift: #right_up
-            return 'p'
-        elif y/2-shift<center_dot[1]<y/2+shift: #right
-            return 'd'
-        else: #right_down
-            return 'l'
+        return 'b'
     elif center_dot[0]<x/2-shift:
-        if center_dot[1]>y/2+shift: #left_up
-            return 'o'
-        elif y/2-shift<center_dot[1]<y/2+shift: #left
-            return 'a'
-        else: #left_down
-            return 'k'
+        return 'a'
     else:
-        if center_dot[1]>y/2+shift: #up
-            return 'w'
-        elif y/2-shift<center_dot[1]<y/2+shift: #stop
-            return
-        else: #down
-            return 's'
+        return 'c'
 
 def crawler(url):
-    rq = urllib.request.Request(url)
-    rq.add_header("user-agent", "mozilla/5.0")
-    webSourceCode = urllib.request.urlopen(rq).read().decode("utf-8", "ignore")
-    contentRe = re.compile(r'<p>(.*?)</p>')
-    content = contentRe.findall(webSourceCode)
-    for c in content:
-        print(c)
-    return content
+    content=requests.get(url)
+    return content.text
+
+def last_pos(pos,content):
+    while(0<=ord(content[pos])-ord('0')<10):
+        pos+=1
+    return pos
+def next_pos(pos,content):
+    while (True):
+        if 0<=ord(content[pos])-ord('0')<10:
+            break
+        else:
+            pos+=1
+    return pos
+def change(content):
+    list=[]
+    fnp=10 #first_num_pos
+    f_pos =l_pos= fnp
+    for i in range(7):
+        f_pos = next_pos(l_pos, content)
+        l_pos = last_pos(f_pos, content)
+        num = eval(content[f_pos:l_pos])
+        list.append(num)
+        print(num)
+    lu=[list[0],list[2]]
+    rd=[list[1],list[3]]
+    length=list[5]
+    width=list[4]
+    return [lu,rd,length,width]
 
 def run():
     action = "empty"
+    url = "http://192.168.43.84/test.html"
     while True:
-        url = "https://www.ittime.com.cn/news/zixun.shtml"
-        dot_list = crawler(url)
-        action = movement(dot_list[0],dot_list[1])
+        content = crawler(url)
+        dot_list = change(content)
+        action = movement(dot_list[0],dot_list[1],dot_list[2],dot_list[3])
+        print(action)
         ser.write(action.encode()) #output to arduino
-        time.sleep(1)
-
+        time.sleep(2)
 
 
 print ('Program begin!')
@@ -65,9 +94,9 @@ for p in ports:
     else :
         print ("No Arduino Device was found connected to the computer")
 
-#ser=serial.Serial(port='COM4')
-#ser=serial.Serial(port='/dev/ttymodem542')
-ser = serial.Serial(port = '/dev/cu.wchusbserial14430')
-#wait 2 seconds for arduino board restart
+# ser=serial.Serial(port='COM4')
+ser=serial.Serial(port='/dev/tty.usbmodem75')
+# ser = serial.Serial(port = '/dev/cu.wchusbserial14430')
+# wait 2 seconds for arduino board restart
 time.sleep(2)
 run()
